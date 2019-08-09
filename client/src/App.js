47cd5a947892e4498f2985091927566e573fbdf5
Virginia Thayer <virginia@stripe.com>
2019-08-09 11:17:05 -0700

    Created new Recipes component to display fetched recipe data outside of form component. User registration post works properly and recipe fetching works properly.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormikLoginForm from './components/LoginForm';
+import Recipes from './components/Recipes'
 
 function App() {
+
+ 
+    
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormikLoginForm />
+      <Recipes />
     </div>
   );
 }
+
+
 
 export default App;
---
+++ client/src/components/LoginForm.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import FormikLoginForm, {LoginForm} from './LoginForm';
+
+describe('<FormikLoginForm />', () => {
+  it('renders without crashing', () => {
+    render(<FormikLoginForm />);
+  });
+  it('One Big Test', () => {
+    const {getByText} = render(<FormikLoginForm />);
+    const strike = getByText(/^strike$/i);
+    const ball = getByText(/^ball$/i);
+    const foul = getByText(/^foul$/i);
+    const hit = getByText(/^hit$/i);
+
+    // Test that strike works
+    fireEvent.click(strike);
+    fireEvent.click(strike);
+    getByText(/Strikes: 2/);
+
+    // Test that ball works
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    getByText(/Balls: 3/);
+
+    // Test that strike rolls over the count
+    fireEvent.click(strike);
+    getByText(/Strikes: 0/);
+    getByText(/Balls: 0/);
+
+    // Test that strike rolls over the count
+    fireEvent.click(strike);
+    fireEvent.click(strike);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    getByText(/Strikes: 0/);
+    getByText(/Balls: 0/);
+
+    // Test that foul doesn't increase strikes past 2
+    fireEvent.click(foul);
+    getByText(/Strikes: 1/);
+    fireEvent.click(foul);
+    getByText(/Strikes: 2/);
+    fireEvent.click(foul);
+    getByText(/Strikes: 2/);
+
+    // Check that hit resets count
+    fireEvent.click(strike);
+    fireEvent.click(ball);
+    fireEvent.click(hit);
+    getByText(/Strikes: 0/);
+    getByText(/Balls: 0/);
+    
+  });
+});
